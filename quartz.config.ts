dcc0a992ed84c5995edb183d3bf95509cf3a0bf9
Rezillique <bogacuzunismail@gmail.com>
2024-12-31 10:01:18 +0300

    Quartz sync: Dec 31, 2024, 10:01 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -48,7 +48,7 @@ const config: QuartzConfig = {
           dark: "#ebebec",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          highlight: "rgba(143, 159, 169, 1)",
           textHighlight: "#b3aa0288",
         },
       },
