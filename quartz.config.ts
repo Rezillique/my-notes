6cccef33b2e20e0c935dab22fef3ab624e924165
Rezillique <bogacuzunismail@gmail.com>
2024-12-31 08:00:45 +0300

    Quartz sync: Dec 31, 2024, 8:00 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: " Rezillique ",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Libre Franklin",
+        body: "Lora",
         code: "IBM Plex Mono",
       },
       colors: {
