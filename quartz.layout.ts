1f1d9fbaec28329e9a40c55ee7c89c23f626d4cc
Rezillique <bogacuzunismail@gmail.com>
2024-12-31 08:54:12 +0300

    Quartz sync: Dec 31, 2024, 8:54 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/Rezillique",
+      "Instagram": "https://www.instagram.com/bogac.io/profilecard/",
     },
   }),
 }
