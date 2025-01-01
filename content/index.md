a4d20663d46e4a79d126cdc11f913c30f710f499
Rezillique <bogacuzunismail@gmail.com>
2025-01-01 04:17:04 +0300

    Quartz sync: Jan 1, 2025, 4:17 AM

--- content/index.md
+++ content/index.md
@@ -10,3 +10,20 @@ Okuyuculara bir uyarı: Karanlık modda okuyun. Lütfen.
 
 
 Şimdiden yolculuğuma eşlik ettiğiniz teşekkürlerimi sunar ve sahneden çıkarım.
+
+
+<script src="https://giscus.app/client.js"
+        data-repo="Rezillique/my-notes"
+        data-repo-id="R_kgDONjvPNw"
+        data-category="Announcements"
+        data-category-id="DIC_kwDONjvPN84ClpQq"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="dark"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
+</script>
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'Rezillique/my-notes',
+        // from data-repo-id
+        repoId: 'R_kgDONjvPNw',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDONjvPN84ClpQq',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/Rezillique",
